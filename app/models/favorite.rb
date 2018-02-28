396c5e48b91fb5ab2b4c7a1c2481d0636d31141b
John Carlo Lopez <condottiere.jc@gmail.com>
2018-02-28 10:57:17 -0500

    fix favorite functionality and main search

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ApplicationRecord
 
   validates :user_id, presence: true
   validates :recipe_id, presence: true
-  validates :created_at, presence: true
+  # validates 'created_at', presence: true
 
   validates :user_id, uniqueness: { scope: [:recipe_id] }
 
