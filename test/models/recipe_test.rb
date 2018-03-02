a88296b4406874afa2250965011b76c19df0f436
ANDREW SEGAL <segal.a@hotmail.com>
2018-03-02 11:47:47 -0500

    added more minor tests, css show page fixes

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
-
 class RecipeTest < ActiveSupport::TestCase
+
+  def test_example_dot_com
+     VCR.use_cassette("avocado21") do
+     response = Recipe.requestapi('avocado')
+     assert_equal(["meals"], response.keys)
+     end
+  end
+
+  test 'A recipe can be created' do
+    recipe = build(:recipe)
+    assert recipe.valid?
+  end
+
+
+
 
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'A user is able to be created' do
+    user = create(:user)
+    assert user.valid?
+  end
+
+  test 'A user must have email' do
+    user = build(:user, email: nil)
+    assert user.invalid?
+  end
+
+  test 'A user must have a name' do
+    user = build(:user, name: nil)
+    assert user.invalid?
+  end
+
 end
